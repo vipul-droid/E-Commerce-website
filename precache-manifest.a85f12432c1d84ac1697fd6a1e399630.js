467ed9acab90dc97d256a28a578ab4711b3fc13a
Vipul Tiwari <vipulengg98@gmail.com>
2021-08-28 22:35:23 +0530

    Updates

---
+++ precache-manifest.a85f12432c1d84ac1697fd6a1e399630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7051ad0de22ae5b86517e1b72164017c",
+    "url": "/E-Commerce-website/index.html"
+  },
+  {
+    "revision": "36468612fc345afcfc9c",
+    "url": "/E-Commerce-website/static/js/2.08791487.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/E-Commerce-website/static/js/2.08791487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97d7a8234e0e919f5ea2",
+    "url": "/E-Commerce-website/static/js/main.c9dde14d.chunk.js"
+  },
+  {
+    "revision": "891bf532ab0018da5305",
+    "url": "/E-Commerce-website/static/js/runtime-main.162cf5e8.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/E-Commerce-website/static/media/commerce.422fec10.png"
+  }
+]);
